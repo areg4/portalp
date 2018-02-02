31b84b4a14a8d1f66f761f8b1ca89f9722434d55
Unknown <mjuarez22@alumnos.uaq.mx>
2018-02-02 13:23:39 -0600

    Tabla responsive, sólo estilos y se agregó campos de prueba

--- application/views/app/fragments/tramitessa/tramites_notificaciones_alumno_fragment.php
+++ application/views/app/fragments/tramitessa/tramites_notificaciones_alumno_fragment.php
@@ -4,25 +4,34 @@
 
 <div class="">
   <table class="table responsive">
-    <tr >
-      <th>Tipo Tramite</th>
-      <th>Estatus</th>
-      <th>Observaciones</th>
-    </tr>
-    <tr class="tr-notifi-alumno" data="1">
-      <td>tt</td>
-      <td>status</td>
-      <td>obser</td>
-    </tr>
-    <tr class="tr-notifi-alumno" data="2">
-      <td>tt</td>
-      <td>status</td>
-      <td>obser</td>
-    </tr>
-    <tr class="tr-notifi-alumno" data="3">
-      <td>tt</td>
-      <td>status</td>
-      <td>obser</td>
-    </tr>
+    <thead>
+      <tr>
+        <th>Tipo de trámite</th>
+        <th>Estatus</th>
+        <th>Observaciones</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="tr-notifi-alumno" data="1">
+        <td>Examen Voluntario</td>
+        <td>En Proceso</td>
+        <td>No hay respuesta sobre la entrega del kardex del alumno</td>
+      </tr>
+      <tr class="tr-notifi-alumno" data="2">
+        <td>tt</td>
+        <td>status</td>
+        <td>obser</td>
+      </tr>
+      <tr class="tr-notifi-alumno" data="3">
+        <td>tt</td>
+        <td>status</td>
+        <td>obser</td>
+      </tr>
+      <tr class="tr-notifi-alumno" data="4">
+        <td>tt</td>
+        <td>status</td>
+        <td>obser</td>
+      </tr>
+    </tbody>
   </table>
 </div>
