8a21bcfdc0b78d3cbb0051eed27702c0a5e6db90
Gerardo Gudiño García <areg_4@hotmail.com>
2018-03-21 18:16:30 -0600

    solución input file

--- application/views/app/fragments/tramitessa/tramites_alta_alumno_fragment.php
+++ application/views/app/fragments/tramitessa/tramites_alta_alumno_fragment.php
@@ -66,7 +66,7 @@
           <p>Kárdex</p>
           <div class="file">
             <img src="<?=base_url()?>static/img/kardex.png" id="2">
-              <input class="ocultar"  id="" type="file" name="kardexEV" required onchange="cambiarIcon(2)">
+              <input class="ocultar"  id="" type="file" name="kardexEV" accept="application/pdf" required onchange="cambiarIcon(2)">
               <input type="hidden" name="idTramite" value="<?=$tramite->idCatTramite; ?>">
               <input type="hidden" name="tramite" value="<?=$tramite->tramite; ?>" >
           </div>
